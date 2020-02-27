8003f06e87fd3c8cd65a6de94f2e569064ad4cfc
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-02-27 21:54:54 +0000

    Created LrpManagement Project

---
+++ src/LRPManagement/LRPManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LRPManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
