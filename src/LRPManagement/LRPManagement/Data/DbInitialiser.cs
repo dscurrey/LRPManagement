0172960588a4a0bca89307d908913ec851c33c88
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-02-28 17:49:55 +0000

    Added DBInitialiser
    Allows for adding seed and/or test data to DB

---
+++ src/LRPManagement/LRPManagement/Data/DbInitialiser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LRPManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LRPManagement.Data
+{
+    public static class DbInitialiser
+    {
+        public static void Initialise(LrpDbContext context)
+        {
+            context.Database.EnsureCreated();
+            //context.Database.Migrate();
+
+            if (context.Skills.Any())
+            {
+                return; // DB Contains skills (is seeded)
+            }
+
+            var skills = new Skill[]
+            {
+                new Skill {Name = "Test Skill 1"},
+                new Skill {Name = "Test Skill 2"}
+            };
+            foreach (Skill s in skills)
+            {
+                context.Skills.Add(s);
+            }
+            context.SaveChanges();
+
+            // TODO - Seed Players
+            // TODO - Seed Characters
+        }
+    }
+}
--- src/LRPManagement/LRPManagement/Program.cs
+++ src/LRPManagement/LRPManagement/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LRPManagement.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,24 @@ namespace LRPManagement
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<LrpDbContext>();
+                    DbInitialiser.Initialise(context);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "A Seeding Error Occured");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
