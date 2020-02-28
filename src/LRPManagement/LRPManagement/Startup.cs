f2b8100d7c05900d1b373f86681defbd34696aac
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-02-28 18:11:58 +0000

    Added logging to startup

--- src/LRPManagement/LRPManagement/Startup.cs
+++ src/LRPManagement/LRPManagement/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace LRPManagement
 {
@@ -44,10 +45,11 @@ namespace LRPManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
+                logger.LogInformation("In Development Environment.");
                 app.UseDeveloperExceptionPage();
             }
             else
