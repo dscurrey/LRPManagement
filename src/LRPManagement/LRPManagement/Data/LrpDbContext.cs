c3c3e274f6ff6d98cbf75c072c487f77aae2e5a7
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-02-28 15:56:43 +0000

    Added Context

---
+++ src/LRPManagement/LRPManagement/Data/LrpDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LRPManagement.Models;
+
+namespace LRPManagement.Data
+{
+    public class LrpDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<CharSkill> CharSkills { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PreReqSkill> PreReqSkills { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+
+        public LrpDbContext(DbContextOptions<LrpDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
--- src/LRPManagement/LRPManagement/Startup.cs
+++ src/LRPManagement/LRPManagement/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LRPManagement.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,20 @@ namespace LRPManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<LrpDbContext>
+            (
+                options => options.UseSqlServer
+                (
+                    Configuration.GetConnectionString("LrpDb")
+                )
+            );
+
             services.AddControllersWithViews();
         }
 
