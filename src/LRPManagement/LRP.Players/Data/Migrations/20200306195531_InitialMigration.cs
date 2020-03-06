a2d1096787713ae01f9be50614dc5481020bbe26
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-03-06 19:55:49 +0000

    Created Players Service

---
+++ src/LRPManagement/LRP.Players/Data/Migrations/20200306195531_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LRP.Players.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    DateJoined = table.Column<DateTime>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Player",
+                columns: new[] { "Id", "DateJoined", "FirstName", "IsActive", "LastName" },
+                values: new object[] { 1, new DateTime(2020, 3, 6, 19, 55, 31, 264, DateTimeKind.Local).AddTicks(9356), "Test", true, "user" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
---
+++ src/LRPManagement/LRP.Players/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LRP.Players.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateJoined { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
