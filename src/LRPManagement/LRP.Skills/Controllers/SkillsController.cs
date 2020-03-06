6c43922c949952e8ce619775947883a95af7be7a
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-03-06 22:20:57 +0000

    Added Missing Controller

---
+++ src/LRPManagement/LRP.Skills/Controllers/SkillsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LRP.Skills.Data;
+using LRP.Skills.Models;
+
+namespace LRP.Skills.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly SkillDbContext _context;
+
+        public SkillsController(SkillDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Skills
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkill()
+        {
+            return await _context.Skill.ToListAsync();
+        }
+
+        // GET: api/Skills/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Skill>> GetSkill(int id)
+        {
+            var skill = await _context.Skill.FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return skill;
+        }
+
+        // PUT: api/Skills/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSkill(int id, Skill skill)
+        {
+            if (id != skill.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(skill).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SkillExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Skills
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Skill>> PostSkill(Skill skill)
+        {
+            _context.Skill.Add(skill);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSkill", new { id = skill.Id }, skill);
+        }
+
+        // DELETE: api/Skills/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Skill>> DeleteSkill(int id)
+        {
+            var skill = await _context.Skill.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skill.Remove(skill);
+            await _context.SaveChangesAsync();
+
+            return skill;
+        }
+
+        private bool SkillExists(int id)
+        {
+            return _context.Skill.Any(e => e.Id == id);
+        }
+    }
+}
