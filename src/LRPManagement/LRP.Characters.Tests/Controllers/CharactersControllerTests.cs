a47556fbbee4e634344265dd5f240ebd2670f199
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-03-06 22:21:03 +0000

    Created Test Stubs

---
+++ src/LRPManagement/LRP.Characters.Tests/Controllers/CharactersControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LRP.Characters.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRP.Characters.Controllers.Tests
+{
+    [TestClass()]
+    public class CharactersControllerTests
+    {
+        [TestMethod()]
+        public void CharactersControllerTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetCharacterTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetCharacterTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void PutCharacterTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void PostCharacterTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void DeleteCharacterTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/LRPManagement/LRP.Skills.Tests/Controllers/SkillsControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LRP.Skills.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRP.Skills.Controllers.Tests
+{
+    [TestClass()]
+    public class SkillsControllerTests
+    {
+        [TestMethod()]
+        public void SkillsControllerTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetSkillTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetSkillTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void PutSkillTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void PostSkillTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void DeleteSkillTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
