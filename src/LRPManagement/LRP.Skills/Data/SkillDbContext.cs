cbdc112b649950b51501705a7aad422b7d87ff2e
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-03-06 21:51:28 +0000

    Created Skills service with API CRUD

---
+++ src/LRPManagement/LRP.Skills/Data/SkillDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LRP.Skills.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace LRP.Skills.Data
+{
+    public class SkillDbContext : DbContext
+    {
+        public virtual DbSet<Skill> Skill { get; set; }
+        private IWebHostEnvironment HostEnv { get; set; }
+
+        public SkillDbContext(DbContextOptions options, IWebHostEnvironment hostEnv) : base(options)
+        {
+            HostEnv = hostEnv;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            if (HostEnv != null && HostEnv.IsDevelopment())
+            {
+                // Seed Data (Dev)
+                builder.Entity<Skill>().HasData(
+                    new Skill { Id = 1, Name = "Weapon Master"},
+                    new Skill { Id = 2, Name = "Artisan"}
+                );
+            }
+        }
+    }
+}
---
+++ src/LRPManagement/LRP.Skills/Data/Skills/ISkillRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LRP.Skills.Models;
+
+namespace LRP.Skills.Data.Skills
+{
+    public interface ISkillRepository
+    {
+        Task<List<Skill>> GetAll();
+        Task<Skill> GetSkill(int id);
+        Task InsertSkill(Skill skill);
+        void DeleteSkill(int id);
+        void UpdateSkill(Skill skill);
+        Task Save();
+    }
+}
