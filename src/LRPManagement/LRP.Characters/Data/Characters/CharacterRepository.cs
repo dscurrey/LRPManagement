edace4390c133eb71d13c384a845c5b38014ec5c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-03-06 21:13:48 +0000

    Created CharacterRepository

---
+++ src/LRPManagement/LRP.Characters/Data/Characters/CharacterRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LRP.Characters.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LRP.Characters.Data.Characters
+{
+    public class CharacterRepository : ICharacterRepository
+    {
+        private readonly CharacterDbContext _context;
+
+        public CharacterRepository(CharacterDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Character>> GetAll()
+        {
+            return await _context.Character.ToListAsync();
+        }
+
+        public async Task<Character> GetCharacter(int id)
+        {
+            return await _context.Character.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task InsertCharacter(Character character)
+        {
+            _context.Character.Add(character);
+        }
+
+        public async void DeleteCharacter(int id)
+        {
+            var character = await _context.Character.FirstOrDefaultAsync(c => c.Id == id);
+            character.IsRetired = true;
+            character.IsActive = false;
+            _context.Character.Update(character);
+        }
+
+        public void UpdateCharacter(Character character)
+        {
+            _context.Entry(character).State = EntityState.Modified;
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/LRPManagement/LRP.Characters/Data/Characters/ICharacterRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LRP.Characters.Models;
+
+namespace LRP.Characters.Data.Characters
+{
+    public interface ICharacterRepository
+    {
+        Task<List<Character>> GetAll();
+        Task<Character> GetCharacter(int id);
+        Task InsertCharacter(Character character);
+        void DeleteCharacter(int id);
+        void UpdateCharacter(Character character);
+        Task Save();
+    }
+}
