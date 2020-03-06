2112264203430789707dc98361cd5ea00d526561
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-03-06 22:23:06 +0000

    Code layout tweaks

--- src/LRPManagement/LRP.Players/Startup.cs
+++ src/LRPManagement/LRP.Players/Startup.cs
@@ -29,7 +29,6 @@ namespace LRP.Players
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<PlayerDbContext>
             (
                 options =>
