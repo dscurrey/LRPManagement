c3920b3483634ec7f1d51d37dbbfbe30456fe67b
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-14 18:36:58 +0100

    Created AuthenticationHandler and userService

---
+++ src/LRPManagement/Authentication/Helpers/BasicAuthenticationHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Authentication.Models;
+using Authentication.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Authentication.Helpers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly IUserService _userService;
+
+        public BasicAuthenticationHandler(IUserService userService, IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        : base(options, logger, encoder, clock)
+        {
+            _userService = userService;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing Auth Header.");
+            }
+
+            User user = null;
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var creds = Encoding.UTF8.GetString(credentialBytes).Split(new[] {':'}, 2);
+
+                var username = creds[0];
+                var password = creds[1];
+
+                user = await _userService.Authenticate(username, password);
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Auth Header.");
+            }
+
+            if (user == null)
+            {
+                return AuthenticateResult.Fail("Invalid User Credentials");
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var ident = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(ident);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+}
---
+++ src/LRPManagement/Authentication/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Authentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authentication.Helpers;
+
+namespace Authentication.Services
+{
+    public class UserService : IUserService
+    {
+
+        private List<User> _users = new List<User>();
+
+        public async Task<User> Authenticate(string uname, string pword)
+        {
+            var user = await Task.Run( () => _users.FirstOrDefault(u => u.Username == uname && u.Password == pword));
+
+            if (user == null)
+            {
+                // User Not found
+                return null;
+            }
+
+            return user.WithoutPassword();
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            return await Task.Run(() => _users.WithoutPasswords());
+        }
+    }
+}
