f52d807ea4d1621d49749bab7daac96c59f4d02c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-14 18:25:05 +0100

    Created interface, User model

---
+++ src/LRPManagement/Authentication/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authentication.Models;
+
+namespace Authentication.Services
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string uname, string pword);
+        Task<List<User>> GetAll();
+    }
+}
