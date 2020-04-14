a548e86bee0b0e0f0c446e63eb5d14fac522fdda
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-14 18:20:08 +0100

    Added extensionmethods

---
+++ src/LRPManagement/Authentication/Helpers/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authentication.Models;
+
+namespace Authentication.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<User> WithoutPasswords(this IEnumerable<User> users)
+        {
+            return users.Select(u => u.WithoutPassword());
+        }
+
+        public static User WithoutPassword(this User user)
+        {
+            user.Password = null;
+            return user;
+        }
+    }
+}
