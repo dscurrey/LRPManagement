280c2da670ffabf4d0549f3735babb5cc2e12ed8
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-15 23:54:12 +0100

    Setup model for jwt

--- src/LRPManagement/Authentication/Models/User.cs
+++ src/LRPManagement/Authentication/Models/User.cs
@@ -12,5 +12,6 @@ namespace Authentication.Models
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Token { get; set; }
     }
 }
