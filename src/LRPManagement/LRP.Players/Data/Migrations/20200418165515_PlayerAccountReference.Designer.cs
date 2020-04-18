e2a8d095da95b2f0f620a6efd83435cd695fffbe
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-18 17:55:26 +0100

    Added migrations

--- src/LRPManagement/LRP.Players/Data/Migrations/20200418165515_PlayerAccountReference.Designer.cs
+++ src/LRPManagement/LRP.Players/Data/Migrations/20200418165515_PlayerAccountReference.Designer.cs
@@ -4,14 +4,16 @@ using LRP.Players.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LRP.Players.Data.Migrations
 {
     [DbContext(typeof(PlayerDbContext))]
-    partial class PlayerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200418165515_PlayerAccountReference")]
+    partial class PlayerAccountReference
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
