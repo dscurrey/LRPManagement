45212d5ce5541868b7057817768837c0f40951c9
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-24 21:47:50 +0100

    Added opening action

--- src/OwlApp/app/src/main/java/uk/co/dcurrey/owlapp/ui/functions/FunctionsFragment.java
+++ src/OwlApp/app/src/main/java/uk/co/dcurrey/owlapp/ui/functions/FunctionsFragment.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -20,11 +22,13 @@ import java.util.List;
 import uk.co.dcurrey.owlapp.R;
 import uk.co.dcurrey.owlapp.database.character.CharacterEntity;
 import uk.co.dcurrey.owlapp.database.character.CharacterViewModel;
+import uk.co.dcurrey.owlapp.ui.character.BondItemActivity;
 import uk.co.dcurrey.owlapp.ui.items.ItemsViewModel;
 
 public class FunctionsFragment extends Fragment
 {
     private CharacterViewModel mCharViewModel;
+    private View root;
 
     public static FunctionsFragment newInstance()
     {
@@ -36,7 +40,7 @@ public class FunctionsFragment extends Fragment
                              @Nullable Bundle savedInstanceState)
     {
         FunctionsViewModel mViewModel = new ViewModelProvider(this).get(FunctionsViewModel.class);
-        View root = inflater.inflate(R.layout.functions_fragment, container, false);
+        root = inflater.inflate(R.layout.functions_fragment, container, false);
 
         TextView mCharCountView = root.findViewById(R.id.charCount);
         mViewModel.getCharCount().observe(getViewLifecycleOwner(), new Observer<Integer>()
@@ -55,6 +59,17 @@ public class FunctionsFragment extends Fragment
     public void onActivityCreated(@Nullable Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
+
+        Button btnBond = root.findViewById(R.id.btnBond);
+        btnBond.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getContext(), BondItemActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
