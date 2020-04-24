3f6df0c9bd6c28c18098ade3e67bf4c4efe60cc4
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-24 16:48:49 +0100

    Tweaked model

--- src/LRPManagement/LRPManagement/Models/Craftable.cs
+++ src/LRPManagement/LRPManagement/Models/Craftable.cs
@@ -12,7 +12,10 @@ namespace LRPManagement.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Type { get; set; }
+        public string Form { get; set; }
+        public string Requirement { get; set; }
+        public string Effect { get; set; }
+        public string Materials { get; set; }
 
         public virtual ICollection<Bond> Bond { get; set; }
     }
