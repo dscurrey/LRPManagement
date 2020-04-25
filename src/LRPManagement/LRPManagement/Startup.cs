c5b9ce8d8f7121df93dc19ecd97f137ec2510c91
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-25 22:58:53 +0100

    Added missing scope

--- src/LRPManagement/LRPManagement/Startup.cs
+++ src/LRPManagement/LRPManagement/Startup.cs
@@ -75,6 +75,7 @@ namespace LRPManagement
             services.AddScoped<ICraftableRepository, CraftableRepository>();
             services.AddScoped<IBondRepository, BondRepository>();
             services.AddScoped<IBondService, BondService>();
+            services.AddScoped<ICharacterSkillService, CharacterSkillService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
