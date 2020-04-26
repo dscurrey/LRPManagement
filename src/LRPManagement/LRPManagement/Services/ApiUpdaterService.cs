bd926a4907d803786b57967a485fd2478f59f9be
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-26 15:20:06 +0100

    Implemented getting characters in service

--- src/LRPManagement/LRPManagement/Services/ApiUpdaterService.cs
+++ src/LRPManagement/LRPManagement/Services/ApiUpdaterService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using LRPManagement.Data.Characters;
 using LRPManagement.Data.Players;
 using LRPManagement.Models;
 using Microsoft.Extensions.Logging;
@@ -15,13 +16,17 @@ namespace LRPManagement.Services
 
         private IPlayerRepository _playerRepository;
         private IPlayerService _playerService;
+        private ICharacterRepository _characterRepository;
+        private ICharacterService _characterService;
 
-        public ApiUpdaterService(ILogger<ApiUpdaterService> logger, IPlayerService playerService, IPlayerRepository playerRepository)
+        public ApiUpdaterService(ILogger<ApiUpdaterService> logger, IPlayerService playerService, IPlayerRepository playerRepository, ICharacterRepository characterRepository, ICharacterService characterService)
         {
             _logger = logger;
 
             _playerService = playerService;
             _playerRepository = playerRepository;
+            _characterService = characterService;
+            _characterRepository = characterRepository;
         }
 
         public async Task DoWork(CancellationToken stoppingToken)
@@ -34,8 +39,7 @@ namespace LRPManagement.Services
                     "API Updater Service is working. Count: {Count}", executionCount);
 
                 await GetPlayers();
-
-
+                await GetCharacters();
 
                 await Task.Delay(interval, stoppingToken);
             }
@@ -66,6 +70,40 @@ namespace LRPManagement.Services
                     }
 
                     await _playerRepository.Save();
+                }
+            }
+            catch (BrokenCircuitException e)
+            {
+                _logger.LogWarning("Broken Circuit");
+            }
+        }
+
+        private async Task GetCharacters()
+        {
+            try
+            {
+                var characters = await _characterService.GetAll();
+                if (characters != null)
+                {
+                    foreach (var character in characters)
+                    {
+                        if (await _characterRepository.GetCharacterRef(character.Id) != null || await _playerRepository.GetPlayer(character.PlayerId) == null)
+                        {
+                            continue;
+                        }
+
+                        var newChar = new Character
+                        {
+                            Name = character.Name,
+                            IsActive = character.IsActive,
+                            IsRetired = character.IsRetired,
+                            CharacterRef = character.Id,
+                            PlayerId = character.PlayerId,
+                            Xp = character.Xp
+                        };
+                        _characterRepository.InsertCharacter(newChar);
+                        await _characterRepository.Save();
+                    }
                 }
             }
             catch (BrokenCircuitException e)
