304e529049856078f73a3dd2daba801a48f2ad4c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-04-26 13:06:50 +0100

    Created new activity

---
+++ src/OwlApp/app/src/main/java/uk/co/dcurrey/owlapp/ui/functions/CharSkillActivity.java
@@ -0,0 +1,18 @@
+package uk.co.dcurrey.owlapp.ui.functions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uk.co.dcurrey.owlapp.R;
+
+public class CharSkillActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_skill);
+    }
+}
