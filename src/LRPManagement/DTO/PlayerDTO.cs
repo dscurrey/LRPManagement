1769776e2f7e0deded3722b9c4e5fe4f1360b267
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-05-05 12:25:24 +0100

    Added display annotation to DTOs

--- src/LRPManagement/DTO/PlayerDTO.cs
+++ src/LRPManagement/DTO/PlayerDTO.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DTO
 {
     public class PlayerDTO
     {
+        [Display(Name = "Player ID")]
         public int Id { get; set; }
+
+        [Display(Name = "Account Reference")]
         public string AccountRef { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Is Active?")]
         public bool IsActive { get; set; }
+
+        [Display(Name = "Date Joined")]
         public DateTime DateJoined { get; set; }
     }
 }
