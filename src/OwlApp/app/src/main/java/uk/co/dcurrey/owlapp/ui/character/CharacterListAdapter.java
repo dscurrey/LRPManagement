571d448efe4cdb04c2707b02c5a53a0aa4741890
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-05-06 14:11:57 +0100

    Added sorting to Players

--- src/OwlApp/app/src/main/java/uk/co/dcurrey/owlapp/ui/character/CharacterListAdapter.java
+++ src/OwlApp/app/src/main/java/uk/co/dcurrey/owlapp/ui/character/CharacterListAdapter.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Comparator;
